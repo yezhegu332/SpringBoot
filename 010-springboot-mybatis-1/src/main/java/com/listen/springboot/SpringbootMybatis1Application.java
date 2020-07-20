1501a35e19b92b95dcba6e09f32803adba960f9b
listen_test <597491117@qq.com>
2020-07-20 10:12:40 +0800

    add module 010-springboot-mybatis-1

---
+++ 010-springboot-mybatis-1/src/main/java/com/listen/springboot/SpringbootMybatis1Application.java
@@ -0,0 +1,13 @@
+package com.listen.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatis1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatis1Application.class, args);
+    }
+
+}
---
+++ 010-springboot-mybatis-1/src/test/java/com/listen/springboot/SpringbootMybatis1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listen.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatis1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
